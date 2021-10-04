497f4d6e6a359da8dcdf6735598b124074479661
Toby Hogan <tobyhogan44@gmail.com>
2021-10-04 17:33:47 +0100

    Initial version

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+let startTimer = () => {
+    console.log("working");
+    var timeLeft = 200;
+
+    while(timeLeft > 0) {
+        console.log("working1");
+        setInterval(() => {
+
+            mins = Math.floor(2000 / 60);
+            secs = (2000 % 60);
+
+            console.log("nice");
+            timer = document.getElementById("timer-display");
+            timer.innerHTML = `${mins}:${secs}`;
+
+            timeleft -= 1;
+
+        }, 1000)
+    };
+};
 
