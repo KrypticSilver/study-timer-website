a146c693548836b9a108de4bc7afbbf1d22e7a53
Toby Hogan <tobyhogan44@gmail.com>
2021-10-16 09:56:50 +0100

    Added features.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-var timing = false;
-var timeWorked = 25 * 60 * 1000;
+var working = false;
+var auxBreaking = false;
+var shortBreaking = false;
+var longBreaking = false;
+
+var workRemaining = 25 * 60 * 1000;
+var auxBreakRemaining = 10 * 60 * 1000;
+var shortBreakRemaining = 6 * 60 * 1000;
+var longBreakRemaining = 15 * 60 * 1000;
+
+var shortBreakCount = 0;
+var longBreakInterval = 4;
+
+var timerMode = "work";
 
 function formatTime(ms) {
     hours = Math.floor(ms / (3600 * 1000));
@@ -26,38 +38,213 @@ function formatTime(ms) {
 }
 
 function delay(ms) {
-    return new Promise((resolve) => {setTimeout(() => {resolve();}, ms);});
+    return new Promise((resolve) => { setTimeout(() => { resolve(); }, ms); });
 }
 
+function changeMode(modeTo) {
+    timerMode = modeTo;
 
+    document.getElementById("work-tab").style.backgroundColor = "#02c99f";
+    document.getElementById("short-break-tab").style.backgroundColor = "#02c99f";
+    document.getElementById("long-break-tab").style.backgroundColor = "#02c99f";
 
-async function checkTimer() {
-    if (timing == false) {
-        timing = true;
+    document.getElementById("work-tab").style.boxShadow = "#02cfa3";
+    document.getElementById("short-break-tab").style.boxShadow = "#02cfa3";
+    document.getElementById("long-break-tab").style.boxShadow = "#02cfa3";
 
-        workButton = document.getElementById("work-button");
-        workButton.style.boxShadow = "none";
-        workButton.style.transform = "translateY(8px)"
-        workButton.innerHTML = "PAUSE"
+    if (modeTo == "work") {
+        tab = document.getElementById("work-tab");
+        tab.style.backgroundColor = "#00e6b4";
+        tab.style.boxShadow = "0px 5px #00f0bc";
+        checkWorkTimer();
 
-        while (timing == true) {
+    } else if (modeTo == "short break") {
+        tab = document.getElementById("short-break-tab");
+        tab.style.backgroundColor = "#00e6b4";
+        tab.style.boxShadow = "0px 5px #00f0bc";
+        checkShortBreakTimer();
+
+    } else if (modeTo == "long break") {
+        tab = document.getElementById("long-break-tab");
+        tab.style.backgroundColor = "#00e6b4";
+        tab.style.boxShadow = "0px 5px #00f0bc";
+        checkLongBreakTimer();
+    }
+}
+
+async function checkWorkTimer() {
+    if (working == false) {
+        working = true;
+
+        if (auxBreaking == true) {
+            checkAuxillaryBreak()
+        }
+
+        timerButton = document.getElementById("timer-button");
+        timerButton.style.boxShadow = "none";
+        timerButton.style.transform = "translateY(8px)"
+        timerButton.innerHTML = "PAUSE"
+
+        while (working == true) {
             await delay(10);
 
             timer = document.getElementById("timer-display");
-            timer.innerHTML = formatTime(timeWorked);
+            timer.innerHTML = formatTime(workRemaining);
 
-            timeWorked -= 10;
-            console.log(timeWorked);
+            workRemaining -= 1000;
+
+            if (workRemaining == 0) {
+                working = false;
+
+                alert("Work Complete :)");
+
+                checkWorkTimer();
+                changeMode("short break");
+
+            }
         };
 
-        console.log("function finished");
+    } else if (shortBreaking == true) {
+        shortBreaking = false;
 
-    } else if (timing == true) {
-        timing = false;
+        timerButton.style.boxShadow = "0px 8px #2c3f63";
+        timerButton.style.transform = "translateY(0px)"
+        timerButton.innerHTML = "WORK"
+    }
+}
 
-        workButton.style.boxShadow = "0px 8px #2c3f63";
-        workButton.style.transform = "translateY(0px)"
-        workButton.innerHTML = "WORK"
+async function checkShortBreakTimer() {
+    if (shortBreaking == false) {
+        shortBreaking = true;
+
+        if (auxBreaking == true) {
+            checkAuxillaryBreak()
+        }
+
+        timerButton = document.getElementById("timer-button");
+        timerButton.style.boxShadow = "none";
+        timerButton.style.transform = "translateY(8px)"
+        timerButton.innerHTML = "PAUSE"
+
+        while (shortBreaking == true) {
+            await delay(10);
+
+            timer = document.getElementById("timer-display");
+            timer.innerHTML = formatTime(shortBreakRemaining);
+
+            shortBreakRemaining -= 1000;
+
+            if (shortBreakRemaining == 0) {
+                shortBreaking = false;
+                shortBreakCount += 1;
+
+                alert("Short Break Over");
+
+                if (shortBreakCount % longBreakInterval == 0) {
+                    changeMode("long break");
+
+                } else {
+                    workRemaining += (25 * 60 * 1000);
+                    changeMode("work");
+
+                }
+
+            }
+        };
+
+    } else if (shortBreaking == true) {
+        shortBreaking = false;
+
+        timerButton.style.boxShadow = "0px 8px #2c3f63";
+        timerButton.style.transform = "translateY(0px)"
+        timerButton.innerHTML = "RESUME BREAK"
+    }
+}
+
+async function checkLongBreakTimer() {
+    if (longBreaking == false) {
+        longBreaking = true;
+
+        if (auxBreaking == true) {
+            checkAuxillaryBreak()
+        }
+
+        timerButton = document.getElementById("timer-button");
+        timerButton.style.boxShadow = "none";
+        timerButton.style.transform = "translateY(8px)"
+        timerButton.innerHTML = "PAUSE"
+
+        while (longBreaking == true) {
+            await delay(10);
+
+            timer = document.getElementById("timer-display");
+            timer.innerHTML = formatTime(longBreakRemaining);
+
+            longBreakRemaining -= 1000;
+
+            if (longBreakRemaining == 0) {
+                longBreaking = false;
+
+                alert("Short Break Over");
+
+                changeMode("work");
+
+            }
+        };
+
+    } else if (longBreaking == true) {
+        longBreaking = false;
+
+        timerButton.style.boxShadow = "0px 8px #2c3f63";
+        timerButton.style.transform = "translateY(0px)"
+        timerButton.innerHTML = "RESUME BREAK"
+    }
+}
+
+async function checkAuxillaryBreak() {
+    if (auxBreaking == false) {
+        auxBreaking = true;
+
+        if (shortBreaking == true) {
+            checkTimer();
+        }
+
+        breakButton = document.getElementById("break-button");
+        breakButton.style.boxShadow = "none";
+        breakButton.style.transform = "translateY(8px)"
+        breakButton.innerHTML = "STOP FREE BREAK";
+
+        while (auxBreaking == true) {
+            await delay(10);
+
+            timer = document.getElementById("break-display");
+            timer.innerHTML = formatTime(auxBreakRemaining);
+
+            auxBreakRemaining -= 10;
+        };
+
+    } else if (auxBreaking == true) {
+        auxBreaking = false;
+
+        breakButton.style.boxShadow = "0px 8px #2c3f63";
+        breakButton.style.transform = "translateY(0px)"
+        breakButton.innerHTML = "START BREAK NOW";
 
     }
-};
+
+
+}
+
+function checkTimer() {
+    if (timerMode == "work") {
+        checkWorkTimer();
+    } else if (timerMode == "short break") {
+        checkShortBreakTimer();
+    } else if (timerMode == "long break") {
+        checkLongBreakTimer();
+    }
+}
+
+
+changeMode("work");
+checkWorkTimer();
